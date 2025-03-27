a6ffd2f36e0b32cb117822566633228aed93b4cf
Blinde16 <blakejlinde@gmail.com>
2025-03-27 13:41:08 -0600

    Adding base files and changing folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css';
-import BookList from './BookList';
+import { CartProvider } from './context/CartContext';
+import CartPage from './pages/CartPage';
+import BooksPage from './pages/BooksPage';
+import PurchaseBookPage from './pages/PurchaseBookPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <BookList />
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<BooksPage />} />
+            <Route path="/projects" element={<BooksPage />} />
+            <Route
+              path="/donate/:projectName/:projectId"
+              element={<PurchaseBookPage />}
+            />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
   );
 }
